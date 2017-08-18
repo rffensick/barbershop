70b44e5c5e685be384157059a61c6ae805a8791b
Eduard <ilyaskin1998@gmail.com>
2017-08-19 05:05:35 +0600

    header done

--- app/js/common.js
+++ app/js/common.js
@@ -12,3 +12,19 @@ $(function() {
 	// $(window).bind('load resize', hf)
 
 });
+
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--closed')) {
+		navMain.classList.remove('main-nav--closed');
+		navMain.classList.add('main-nav--opened');
+	} else {
+		navMain.classList.add('main-nav--closed');
+		navMain.classList.remove('main-nav--opened');
+	}
+})
