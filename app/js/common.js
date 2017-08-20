3cf2d785f0c574ec13a352c37bfe1a39eee09661
Eduard <ilyaskin1998@gmail.com>
2017-08-21 02:25:32 +0600

    popup done

--- app/js/common.js
+++ app/js/common.js
@@ -13,9 +13,13 @@ $(function() {
 
 });
 
+// MENU
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var openMenu = document.querySelector('.main-nav__user-login');
+var popupLogin = document.querySelector('.popup-login');
+var closeBtn = document.querySelector('.popup-login__close-btn');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -28,3 +32,46 @@ navToggle.addEventListener('click', function() {
 		navMain.classList.remove('main-nav--opened');
 	}
 })
+
+navToggle.addEventListener('click', function() {
+	if (popupLogin.classList.contains('popup-login--opened')) {
+		popupLogin.classList.remove('popup-login--opened');
+		popupLogin.classList.add('popup-login--closed');
+	} else {
+		popupLogin.classList.add('popup-login--closed');
+		popupLogin.classList.remove('popup-login--opened');
+	}
+})
+
+// END MENU
+
+// POPUP!
+
+
+
+openMenu.addEventListener('click', function(e) {
+	e.preventDefault();
+	if (popupLogin.classList.contains('popup-login--closed')) {
+		popupLogin.classList.remove('popup-login--closed');
+		popupLogin.classList.add('popup-login--opened');
+		navMain.classList.remove('main-nav--opened');
+		navMain.classList.add('main-nav--closed');
+	}	else {
+		popupLogin.classList.add('popup-login--closed');
+		popupLogin.classList.remove('popup-login--opened');
+		navMain.classList.add('main-nav--opened');
+		navMain.classList.remove('main-nav--closed');
+	}
+});
+
+closeBtn.addEventListener('click', function() {
+	if (popupLogin.classList.contains('popup-login--opened')) {
+		popupLogin.classList.remove('popup-login--opened');
+		popupLogin.classList.add('popup-login--closed');
+	} else {
+		popupLogin.classList.add('popup-login--opened');
+		popupLogin.classList.remove('popup-login--closed');
+	}
+});
+
+// END POPUP
